973824ada8034eea9d1ade9d4f8db63395fb52ac
Rodrigo W <rodrigowerberich@hotmail.com>
2018-07-03 19:31:48 -0300

    Corrigindo uns erros

--- mainwindowviewmodel.cpp
+++ mainwindowviewmodel.cpp
@@ -155,7 +155,7 @@ void MainWindowViewModel::plan(PlannerInterface * planner_interface, EnvInterfac
          std::cout << "No solution found" << std::endl;
 }
 
-void benchmark(std::vector<PlannerInterface*> planners_interface, EnvInterface* environment_interface, Point start_point, Point goal_point){
+void MainWindowViewModel::benchmark(std::vector<PlannerInterface*> planners_interface, EnvInterface* environment_interface, Point start_point, Point goal_point){
     // construct the state space we are planning in
     auto space(std::make_shared<ob::RealVectorStateSpace>(2));
 
@@ -190,11 +190,14 @@ void benchmark(std::vector<PlannerInterface*> planners_interface, EnvInterface* 
 
     ompl::geometric::SimpleSetup ss(si);
 
+    ss.setStartState(start);
+    ss.setGoalState(goal);
+
     // First we create a benchmark class:
     ompl::tools::Benchmark b(ss, "my experiment");
 
     for(auto * planner_interface:planners_interface){
-        b.addPlanner(planner_interface->getPlanner());
+        b.addPlanner(planner_interface->getPlanner(si));
     }
 
     // Now we can benchmark: 5 second time limit for each plan computation,
@@ -207,4 +210,5 @@ void benchmark(std::vector<PlannerInterface*> planners_interface, EnvInterface* 
     req.runCount = 10;
     req.displayProgress = true;
     b.benchmark(req);
+    b.saveResultsToFile("NomeLouco.log");
 }
